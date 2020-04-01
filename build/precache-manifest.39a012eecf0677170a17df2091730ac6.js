ba25d8e6f141510f4a9d00a9698551688b0e7f14
Paloma Monteiro <paloma.l.monteiro@gmail.com>
2020-04-01 20:42:41 -0300

    Deploy

---
+++ build/precache-manifest.39a012eecf0677170a17df2091730ac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75433810319caa4574d70c92fffa41b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e515a14628f5bded6ca0",
+    "url": "/static/js/2.c9ab243a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c9ab243a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906a9d0a3108b5f88151",
+    "url": "/static/js/main.f6ddc8a9.chunk.js"
+  },
+  {
+    "revision": "d601ed1aeb4842cff21d",
+    "url": "/static/js/runtime-main.e2af74f1.js"
+  }
+]);
